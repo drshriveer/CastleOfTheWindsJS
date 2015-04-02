1214b913a050ec8cc7267c9f35d663e56c612250
Gavin Shriver <gs@Gavins-MacBook-Pro.local>
2015-04-01 21:36:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Castle Of The Winds Javscript Port
+
+This is the start of a Castle Of The Winds Javascript port created by [DrShriveer](http://github.com/drshriveer) and [RobertBowie](http://github.com/RobertBowie).
+
+It has only just begun.... So please be patient. 
---
+++ js/spells.js
@@ -0,0 +1,21 @@
+// This file only contains lists of spells and their actions / attributes
+if(window.app == null ||
+   window.app == undefined) {
+  window.app = {};
+}
+
+var SPELLS = {
+  ARROW: {
+    name: "ARROW",
+    status: ,
+    action: function() {
+      alert("action!");
+    }
+  },
+
+  COLD_BOLT: {
+
+  }
+
+};
+
