745e8d2aaf8736c0d3258dac7bb597370e3a3411
RobertBowie <robertb1903@gmail.com>
2015-04-10 18:55:56 -0700

    Updates mapTemplates.js and tiles.js
    Adds first village to mapTemplates.js.  Adds tile types to tiles.js.

--- js/maps/tiles.js
+++ js/maps/tiles.js
@@ -1,32 +1,125 @@
-// This file only contains lists of spells and their actions / attributes
+// This file only contains lists of tiles and their actions / attributes
 if(window.app == null ||
    window.app == undefined) {
   window.app = {};
 }
 
 var TILES = {
+// Basic Environment Tiles
   DIRT: {
     name: "DIRT",
     imageUrl: "", 
     solid: false,
     action: null
-    info: 
+    info: "Just some dirt",
   },
 
+  GRASS: {
+    name: "GRASS",
+    imageUrl: "", 
+    solid: false,
+    action: null
+    info: "Just some grass",
+  },
+
+  RIVER: {
+    name: "RIVER",
+    imageUrl: "", 
+    solid: true,
+    action: null
+    info: "Just some water",
+  },
+
+  GARDEN: {
+    name: "GARDEN",
+    imageUrl: "",
+    solid: false,
+    action: null,
+    info: "A small garden"
+  }
+
+  WELL: {
+    name: "WELL",
+    imageUrl: "",
+    solid: false,
+    action: null,
+    info: "A small well"
+  }
+
+// Fixture Tiles
   SHOP_DOOR: {
     name: "DIRT",
     imageUrl: "", 
     solid: false,
     action: function(){
-
+      // Open Shop?
     }
+    info: "",
   },
 
-  DOOR: {
-    name: "DOOR".
+  CLOSED_DOOR: {
+    name: "CLOSED_DOOR".
+    imageUrl: "", 
+    solid: true,
+    action: null // Need an action to replace with open door or alert "Locked!"?
+    info: "A closed door",
+  },
+/*
+  LOCKED_DOOR: {  // May not need?
+    name: "LOCKED_DOOR".
+    imageUrl: "", 
+    solid: true,
+    action: null // Need an action to replace with open door or alert "Locked!"?
+    info: "A locked door",
+  },
+*/
+  OPEN_DOOR: {
+    name: "OPEN_DOOR".
     imageUrl: "", 
     solid: false,
     action: null
-  }
-};
+    info: "A door that is standing open",
+  },
+
+  BROKEN_DOOR: {
+    name: "BROKEN_DOOR".
+    imageUrl: "", 
+    solid: false,
+    action: null
+    info: "A door that looks to have been staved in",
+  },
+
+  WALL: {
+    name: "WALL",
+    imageUrl: "", 
+    solid: true,
+    action: null
+    info: "An impassable wall",
+  },
+
+  SIGN: {
+    name: "SIGN",
+    imageUrl: "", 
+    solid: false,
+    action: null // Maybe the action can be a lookup of the closest shop door for sign text?
+    info: "A shop sign", // Will need to incorporate the ability to have diff sign text for each
+  },
+
+  HOUSE: { // This will prob need to be 4-10(?) separate tiles
+    name: "HOUSE",
+    imageUrl: "", 
+    solid: true,
+    action: null
+    info: "A house",
+  },
+
+  ARCH: { // This will prob need to be 3 separate tiles
+    name: "ARCH",
+    imageUrl: "", 
+    solid: true,
+    action: null
+    info: "An Impressive Arch",
+  },
+
+}; // Final brace and semi
 
