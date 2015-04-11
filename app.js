e85c3c14a7baed4e1ca1e76db734c1f641b91584
Gavin Shriver <gavin.shriver@gmail.com>
2015-04-11 01:24:36 -0700

    gets server working properly

--- app.js
+++ app.js
@@ -34,23 +34,30 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.status(err.status || 500)
+        .send('error', {
+          message: err.message,
+          error: err
+        });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.status(err.status || 500)
+      .send('error', {
+        message: err.message,
+        error: {}
+      });
 });
 
-
 module.exports = app;
+
+if (app.get('env') === 'production') {
+  console.log("Starting server in production on port 80.");
+  app.listen(80);
+} else {
+  console.log("Starting server in development on port 3000.");
+  app.listen(3000);
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('root');
+  res.send('index.html');
 });
 
 module.exports = router;
